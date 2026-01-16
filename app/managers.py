15395d18f868ea8392d3f6fcc7f3e07bff71ceaf
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2026-01-16 17:25:55 -0300

    Solution

--- app/managers.py
+++ app/managers.py
@@ -1,16 +1,14 @@
 import sqlite3
-from models import Actor
+from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.connection = sqlite3.connect(db_name)
         self.table_name = table_name
-        # Optional: Ensure the table exists with correct columns
         self._create_table()
 
-    def _create_table(self):
-        """Helper method to ensure the table structure is present."""
+    def _create_table(self) -> None:
         query = f"""
         CREATE TABLE IF NOT EXISTS {self.table_name} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,28 +20,33 @@ class ActorManager:
         self.connection.commit()
 
     def create(self, first_name: str, last_name: str) -> None:
-        """Creates a new entry in the actor table."""
-        query = f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)"
+        query = (
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)"
+        )
         self.connection.execute(query, (first_name, last_name))
         self.connection.commit()
 
     def all(self) -> list[Actor]:
-        """Returns a list of Actor instances from the DB."""
         cursor = self.connection.cursor()
         query = f"SELECT id, first_name, last_name FROM {self.table_name}"
         cursor.execute(query)
         rows = cursor.fetchall()
 
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        return [
+            Actor(id=row[0], first_name=row[1], last_name=row[2])
+            for row in rows
+        ]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
-        """Updates entry with given pk."""
-        query = f"UPDATE {self.table_name} SET first_name = ?, last_name = ? WHERE id = ?"
+        query = (
+            f"UPDATE {self.table_name} SET first_name = ?, last_name = ? "
+            "WHERE id = ?"
+        )
         self.connection.execute(query, (new_first_name, new_last_name, pk))
         self.connection.commit()
 
     def delete(self, pk: int) -> None:
-        """Deletes entry with given pk from DB."""
         query = f"DELETE FROM {self.table_name} WHERE id = ?"
         self.connection.execute(query, (pk,))
         self.connection.commit()
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Actor:
     id: int
     first_name: str
-    last_name: strgit commit -am 'Solution'
+    last_name: str
